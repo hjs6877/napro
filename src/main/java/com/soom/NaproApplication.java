c2abdc6d48ae873e799ceb01aba3d9459b1e0d88
Kevin Hwang <hjs6877@gmail.com>
2017-03-27 18:31:10 +0900

    Initial commit

---
+++ src/main/java/com/soom/NaproApplication.java
@@ -0,0 +1,12 @@
+package com.soom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaproApplication.class, args);
+	}
+}
